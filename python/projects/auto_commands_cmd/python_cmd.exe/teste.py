47f22e375df301a6fa94d27b64d602cddaa88890
luiz-pr <76491544+luiz-pr@users.noreply.github.com>
2021-07-20 19:49:15 -0300

    Adicionado e modificado varios arquivos de uma vez só

---
+++ python/projects/auto_commands_cmd/python_cmd.exe/teste.py
@@ -0,0 +1,68 @@
+import pyautogui
+import time
+import os
+
+
+# Abrir app
+os.startfile('cmd.exe')
+
+# Esperar
+time.sleep(2)
+
+# Escrever no app
+pyautogui.write('title Luiz')
+
+# Apertar tecla enter
+pyautogui.keyDown('enter')
+
+# Soltar a tecla enter
+pyautogui.keyUp('enter')
+
+# Esperar
+time.sleep(2)
+
+
+# Escrever no app
+pyautogui.write('Python')
+
+# Apertar tecla enter
+pyautogui.keyDown('enter')
+
+# Soltar a tecla enter
+pyautogui.keyUp('enter')
+
+# Esperar
+time.sleep(2)
+
+# Escrever no app
+pyautogui.write("print('Hello World!...')")
+
+# Apertar tecla enter
+pyautogui.keyDown('enter')
+
+# Soltar a tecla enter
+pyautogui.keyUp('enter')
+
+# Esperar
+time.sleep(2)
+
+# Escrever no app
+pyautogui.write("print('Ola meu nome e Luiz')")
+
+# Apertar tecla enter
+pyautogui.keyDown('enter')
+
+# Soltar a tecla enter
+pyautogui.keyUp('enter')
+
+# Esperar
+time.sleep(2)
+
+# Apertar tecla alt
+pyautogui.keyDown('alt')
+
+# Pressionar  a tecla f4
+pyautogui.press('f4')
+
+ # Soltar a tecla alt
+pyautogui.keyUp('alt')
